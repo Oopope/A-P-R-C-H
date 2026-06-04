b56f6dc6a1926f5c676a38e4d1ac49ce9a996ea3
Lopencreto <alysilva414@gmail.com>
2026-06-04 10:44:30 -0400

    Reorganización de estructura de archivos y carpetas

--- src/data/conectar.py
+++
@@ -1,7 +0,0 @@
-#esto es la conexion de la base de datos al sistema
-import sqlite3
-conexion = sqlite3.connect('base de dato')
-cursor = conexion.cursor()
-conexion.commit()
-print(cursor.fetchall())
-conexion.close()
